c41aa44be7d40d94d24d1b155170714993f7e1e0
ben@corp <128577990+bencoronard@users.noreply.github.com>
2026-09-26 22:45:31 +0700

    Add HEAD handler in file controller

--- src/main/java/dev/hireben/spring/reactive/web/demo/storage/controller/FileController.java
+++ src/main/java/dev/hireben/spring/reactive/web/demo/storage/controller/FileController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -19,6 +20,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/files/{id}")
 @RequiredArgsConstructor
 final class FileController {
+
+  @RequestMapping(method = RequestMethod.HEAD)
+  Mono<ResponseEntity<Flux<DataBuffer>>> infoHandler(
+      @PathVariable String id) {
+    // If-None-Match: eTag
+    // If-Modified-Since: lastModified
+    return Mono.just(ResponseEntity
+        .ok()
+        .build());
+  }
 
   @GetMapping
   Mono<ResponseEntity<Flux<DataBuffer>>> downloadHandler(
